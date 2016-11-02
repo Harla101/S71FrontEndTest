bf3c63ab2a144f2a075a45b36f8bd5d7feab1680
Steve Harlan <StevenHarlan@gmail.com>
2016-11-02 16:14:36 -0700

    sorting functionality completed

--- src/App.js
+++ src/App.js
@@ -8,22 +8,80 @@ const moment = require('moment')
 class App extends Component {
   constructor(props) {
     super(props)
-    this.state={tableData: []}
+    this.state={
+      tableData: []
+    }
+    this._sortBy = this._sortBy.bind(this)
+    this.sortDirection = null;
+
   }
 
   componentDidMount() {
     this.setState({tableData: this._getTableData()})
-    console.log(this.state.tableData)
   }
 
   componentDidUpdate() {
-    console.log('updated')
   }
 
   _getTableData(){
     // would do external fetch here for API, but mocking with local json for now
     return mockData.channels
   }
+
+  _sortBy(e) {
+    const sortBy = e.target.innerHTML
+    this.sortDirection === 'ascend'? this.sortDirection='descend': this.sortDirection='ascend'
+    console.log(this.sortDirection)
+    switch (sortBy) {
+      case 'Title':
+        this._sortTableDataByTitle()
+        break;
+      case 'Views':
+      this._sortTableData('views')
+        break;
+      case 'Date Created':
+      this._sortTableData('created_on')
+        break;
+      case 'Favorite':
+      this._sortTableData('title')
+        break;
+      default:
+        console.log("nothing valid")
+    }
+  }
+
+// Needed to handle title distinctly for string sorting
+  _sortTableDataByTitle(){
+    let valuesToSort = this.state.tableData.map(data => data);
+    valuesToSort.sort((a,b)=> {
+      if(a['title'].toUpperCase() > b['title'].toUpperCase()){
+        return this.sortDirection === 'ascend'? 1:-1;
+      }
+      if(a['title'].toUpperCase() < b['title'].toUpperCase()){
+        return this.sortDirection === 'ascend'? -1:1;
+      }
+        return 0;
+    })
+    this.setState({tableData: valuesToSort})
+  }
+
+  // Sorting handled for non-string columns
+
+  _sortTableData(byColumn){
+    let valuesToSort = this.state.tableData.map(data => data);
+    valuesToSort.sort((a,b)=> {
+      if(a[byColumn]> b[byColumn]){
+        return this.sortDirection === 'ascend'? 1:-1;
+      }
+      if(a[byColumn] < b[byColumn]){
+        return this.sortDirection === 'ascend'? -1:1;
+      }
+        return 0;
+    })
+    this.setState({tableData: valuesToSort})
+  }
+
+
 
   render() {
     let tableRows = this.state.tableData.map(row =>
@@ -44,11 +102,11 @@ class App extends Component {
           <table>
             <tbody>
               <tr>
-                <th>Thumbnail</th>
-                <th>Title</th>
-                <th>Views</th>
-                <th>Date Created</th>
-                <th>Favorite</th>
+                <th onClick={this._sortBy}>Thumbnail</th>
+                <th onClick={this._sortBy}>Title</th>
+                <th onClick={this._sortBy}>Views</th>
+                <th onClick={this._sortBy}>Date Created</th>
+                <th onClick={this._sortBy}>Favorite</th>
               </tr>
                 {tableRows}
             </tbody>

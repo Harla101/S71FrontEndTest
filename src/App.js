edb3ddfb978559f89cdd24d1575b99a34f0ec3d4
Steve Harlan <StevenHarlan@gmail.com>
2016-11-02 11:13:28 -0700

    removed react logo; html base structure started

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+        <div className="search-bar-container">
+          <form>
+            Filter Results:
+            <input type="search" name="video-search"></input>
+          </form>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className='results-container'>
+          <table>
+            <tr>
+              <th>Thumbnail</th>
+              <th>Title</th>
+              <th>Views</th>
+              <th>Date Created</th>
+              <th>Favorite</th>
+            </tr>
+            <td><img alt='felicia Ricci thumnail' src='https://yt3.ggpht.com/-MdLmmFh3qjo/AAAAAAAAAAI/AAAAAAAAAAA/lcGE1l_gam4/s88-c-k-no/photo.jpg'></img></td>
+            <td>Felicia Ricci</td>
+            <td>3,106,606</td>
+            <td> June 6, 2009 6:53AM</td>
+            <td> false </td>
+          </table>
+        </div>
       </div>
     );
   }

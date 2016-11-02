72544a4b2f75380765ea3764e1381441b58bce39
Steve Harlan <StevenHarlan@gmail.com>
2016-11-02 16:30:09 -0700

    add commas to view numbers function added

--- src/App.js
+++ src/App.js
@@ -81,12 +81,30 @@ class App extends Component {
     this.setState({tableData: valuesToSort})
   }
 
+  _prettyNumber(num) {
+    let value= [];
+    const numLength = num.toString().length;
+    if (numLength < 4) return num;
+    let counter = 0;
+    for(let i = numLength - 1; i >= 0; i--){
+      if(counter % 3 === 0 && counter !== 0){
+        value.unshift(',');
+        value.unshift(num.toString().charAt(i))
+        counter++;
+      } else {
+        value.unshift(num.toString().charAt(i))
+        counter++;
+      }
+    }
+    return value.join('')
+  }
+
 
 
   render() {
     let tableRows = this.state.tableData.map(row =>
       <TableRow key={row.id} id={row.id} imageSrc={row.thumb_url_default}
-                title={row.title} views={row.views}
+                title={row.title} views={this._prettyNumber(row.views)}
                 createdDate={moment(row.created_on).format('MMM D, YYYY h:mm A')}>
       </TableRow>)
     return (

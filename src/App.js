b38d0077c5a186889c42790713791eea8866f27d
Steve Harlan <StevenHarlan@gmail.com>
2016-11-03 09:11:43 -0700

    favoriting functionality added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
 import TableRow from './components/tablerowcomponent';
-import mockData from './mockData/data-mock.json';
 
 const moment = require('moment')
 
@@ -9,50 +8,53 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state={
-      tableData: []
+      APIData: [],
+      visibleTableData:[],
+      favoriteIDs: {}
     }
-    this._sortBy = this._sortBy.bind(this)
+    this._sortBy = this._sortBy.bind(this);
+    this._handleFavorite = this._handleFavorite.bind(this);
     this.sortDirection = null;
-
+    this.currentSort = null;
   }
 
   componentDidMount() {
-    this.setState({tableData: this._getTableData()})
-  }
-
-  componentDidUpdate() {
-  }
-
-  _getTableData(){
-    // would do external fetch here for API, but mocking with local json for now
-    return mockData.channels
+    const self = this;
+    fetch('data-mock.json')
+    .then(function(response) {
+       return response.json()
+     }).then(function(json) {
+       self.setState({APIData: json.channels, visibleTableData: json.channels})
+     }).catch(function(ex) {
+       console.log('parsing failed', ex)
+     })
   }
 
   _sortBy(e) {
-    const sortBy = e.target.innerHTML
+    if (e) this.currentSort=e.target.innerHTML
     this.sortDirection === 'ascend'? this.sortDirection='descend': this.sortDirection='ascend'
-    console.log(this.sortDirection)
-    switch (sortBy) {
+    switch (this.currentSort) {
       case 'Title':
         this._sortTableDataByTitle()
         break;
       case 'Views':
-      this._sortTableData('views')
+        this._sortTableData('views')
         break;
       case 'Date Created':
-      this._sortTableData('created_on')
+        this._sortTableData('created_on')
         break;
       case 'Favorite':
-      this._sortTableData('title')
+        this._sortTableData('title')
         break;
       default:
         console.log("nothing valid")
+        break;
     }
   }
 
-// Needed to handle title distinctly for string sorting
+// Needed to handle Title distinctly for string sorting
   _sortTableDataByTitle(){
-    let valuesToSort = this.state.tableData.map(data => data);
+    let valuesToSort = this.state.visibleTableData.map(data => data);
     valuesToSort.sort((a,b)=> {
       if(a['title'].toUpperCase() > b['title'].toUpperCase()){
         return this.sortDirection === 'ascend'? 1:-1;
@@ -62,13 +64,12 @@ class App extends Component {
       }
         return 0;
     })
-    this.setState({tableData: valuesToSort})
+    this.setState({visibleTableData: valuesToSort})
   }
 
   // Sorting handled for non-string columns
-
   _sortTableData(byColumn){
-    let valuesToSort = this.state.tableData.map(data => data);
+    let valuesToSort = this.state.visibleTableData.map(data => data);
     valuesToSort.sort((a,b)=> {
       if(a[byColumn]> b[byColumn]){
         return this.sortDirection === 'ascend'? 1:-1;
@@ -78,7 +79,7 @@ class App extends Component {
       }
         return 0;
     })
-    this.setState({tableData: valuesToSort})
+    this.setState({visibleTableData: valuesToSort})
   }
 
   _prettyNumber(num) {
@@ -99,34 +100,61 @@ class App extends Component {
     return value.join('')
   }
 
+  _handleSearch(e) {
+    let rowsToShow = this.state.APIData.filter(data => data.title.toUpperCase().includes(e.target.value.toUpperCase()));
+      this.setState({visibleTableData: rowsToShow});
+  }
 
+  _handleSubmit(e){
+    e.preventDefault();
+    return false;
+  }
+
+
+// Would also do a POST to store user favorites to backend DB
+  _handleFavorite(e){
+    e.persist();
+    this.setState(previousState=>{
+      if(previousState['favoriteIDs'][e.target.id]){
+        delete previousState['favoriteIDs'][e.target.id]
+        return previousState
+      } else {
+        previousState['favoriteIDs'][e.target.id] = true;
+      }
+    });
+  }
 
   render() {
-    let tableRows = this.state.tableData.map(row =>
+    let tableRows = [];
+    if(this.state.visibleTableData){  tableRows = this.state.visibleTableData.map(row =>
       <TableRow key={row.id} id={row.id} imageSrc={row.thumb_url_default}
                 title={row.title} views={this._prettyNumber(row.views)}
-                createdDate={moment(row.created_on).format('MMM D, YYYY h:mm A')}>
+                createdDate={moment(row.created_on).format('MMM D, YYYY h:mm A')}
+                handleFavorite={this._handleFavorite}
+                isFavorited={this['state']['favoriteIDs'][row.id]? true:false}
+                >
       </TableRow>)
+    }
     return (
       <div className="App">
         <header> Studio71 Channels</header>
         <div className="search-bar-container">
-          <form>
+          <form onSubmit={this._handleSubmit.bind(this)}>
             Filter Results:
-            <input type="search" name="video-search"></input>
+            <input type="text" name="video-search" onKeyUp={this._handleSearch.bind(this)}></input>
           </form>
         </div>
         <div className='results-container'>
           <table>
             <tbody>
-              <tr>
-                <th onClick={this._sortBy}>Thumbnail</th>
-                <th onClick={this._sortBy}>Title</th>
-                <th onClick={this._sortBy}>Views</th>
-                <th onClick={this._sortBy}>Date Created</th>
-                <th onClick={this._sortBy}>Favorite</th>
+              <tr className='table-headers'>
+                <th></th>
+                <th onClick={this._sortBy} className='sortable'>Title</th>
+                <th onClick={this._sortBy} className='sortable'>Views</th>
+                <th onClick={this._sortBy} className='sortable'>Date Created</th>
+                <th onClick={this._sortBy} className='sortable'>Favorite</th>
               </tr>
-                {tableRows}
+                {(tableRows.length === 0)? <tr><td>No Results</td></tr>: tableRows}
             </tbody>
           </table>
         </div>
--- src/components/tablerowcomponent.js
+++ src/components/tablerowcomponent.js
@@ -1,14 +1,40 @@
 import React from 'react';
 
-const TableRow = (props) => {
-  return(
+class TableRow extends React.Component {
+
+  //prevents unchanged table rows from re-rendering
+  shouldComponentUpdate(nextProps, nextState) {
+    if (this.props.isFavorited !== nextProps.isFavorited){
+      return true;
+    }
+    return false;
+  }
+
+  render(){
+    let divStyle = {
+      color: 'blue'
+    };
+
+    if(this.props.isFavorited){
+      divStyle.color = 'red';
+    } else {
+      divStyle.color = 'white';
+    }
+
+    return (
     <tr>
-      <td><img alt={props.title + ' thumbnail'} src={props.imageSrc}></img></td>
-      <td>{props.title}</td>
-      <td>{props.views}</td>
-      <td> {props.createdDate}</td>
+      <td><img alt={this.props.title + ' thumbnail'} src={this.props.imageSrc}></img></td>
+      <td>{this.props.title}</td>
+      <td>{this.props.views}</td>
+      <td>{this.props.createdDate}</td>
+      <td >
+        <i className="fa fa-heart fa-2x" aria-hidden="true" style={divStyle}
+          onClick={this.props.handleFavorite.bind(this)} id={this.props.id}>
+        </i>
+      </td>
     </tr>
-  )
-};
+    )
+  }
+}
 
 export default TableRow;
